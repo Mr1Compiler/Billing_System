edc24d9e8c6e312fb3952ed7daae6e7e68470d35
mr1compiler <mr1compiler@gmail.com>
2025-06-19 16:24:46 +0300

    Basic structure

---
+++ src/BillingSystem.Application/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace BillingSystem.Application;
+
+public class Class1
+{
+    public int code { get; set; }
+    public Class1(int par1, int par2) => if(par1 > par2) ? par1 par2
+    
+    
+    
+}
---
+++ src/BillingSystem.Persistence/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace BillingSystem.Persistence;
+
+public class Class1
+{
+
+}
