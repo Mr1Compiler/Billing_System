c4f66cd6a8fe304d4c3ab3a3efe30e950ca06ccd
mr1compiler <mr1compiler@gmail.com>
2025-10-10 13:36:09 +0300

    Stop tracking appsettings.Development.json, and fix SubscriptionPlanCreateDto.cs

--- src/Core/BillingSystem.Application/DTOs/V1/SubscriptionPlans/SubscriptionPlanCreateDto.cs
+++ src/Core/BillingSystem.Application/DTOs/V1/SubscriptionPlans/SubscriptionPlanCreateDto.cs
@@ -4,6 +4,7 @@ namespace BillingSystem.Application.DTOs.V1.SubscriptionPlans;
 
 public class SubscriptionPlanCreateDto
 {
+    public Guid Id { get; private set; }
     public string Name { get; set; } = null!;
     public decimal Price { get; set; }
     public string? Description { get; set; }
